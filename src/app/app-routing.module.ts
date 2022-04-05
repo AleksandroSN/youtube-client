546b3144b6926b8b5627966b3bf8b6001964f418
SemyonAleksandrov <aleksandrosemnik@gmail.com>
2022-04-05 12:32:56 +0300

    feat: implement markup for 'not-found-page'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { SearchResultPageComponent } from "@app/youtube/pages";
+import {
+  SearchResultPageComponent,
+  DetailPageComponent,
+} from "@app/youtube/pages";
 import { LoginPageComponent } from "@app/auth/pages";
 import { NotFoundPageComponent } from "@app/core/pages";
 
 const routes: Routes = [
   {
-    path: "",
+    path: "home",
     component: SearchResultPageComponent,
+  },
+  {
+    path: "home/:id",
+    component: DetailPageComponent,
   },
   {
     path: "login",
     component: LoginPageComponent,
   },
+  { path: "", redirectTo: "/home", pathMatch: "full" },
   {
     path: "**",
     component: NotFoundPageComponent,
